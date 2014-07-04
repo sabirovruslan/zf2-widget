7c41fe43c16c4cbb7d3c8683fe27e5fe50fc038f
Renato Cassino <renatocassino@gmail.com>
2014-07-04 02:14:02 -0300

    Widget for ZF2

--- src/TokenPost/Widget/Widget.php
+++ src/TokenPost/Widget/Widget.php
@@ -17,6 +17,15 @@ namespace TokenPost\Widget;
 class Widget
 {
     /**
+     * @var Widget[] the widgets that are currently being rendered (not ended). This property
+     * is maintained by [[begin()]] and [[end()]] methods.
+     * @internal
+     */
+    public static $stack = [];
+    
+    public static $objectConfig = [];
+
+    /**
 	 * Begins a widget.
 	 * This method creates an instance of the calling class. It will apply the configuration
 	 * to the created instance. A matching [[end()]] call should be called later.
@@ -27,8 +36,9 @@ class Widget
 	{
 		$config['class'] = get_called_class();
 		/** @var Widget $widget */
-		$widget = Yii::createObject($config);
+		$widget = self::createObject($config);
 		self::$stack[] = $widget;
+		$widget->init();
 		return $widget;
 	}
 
@@ -46,13 +56,13 @@ class Widget
 				$widget->run();
 				return $widget;
 			} else {
-				throw new InvalidCallException("Expecting end() of " . get_class($widget) . ", found " . get_called_class());
+				throw new \Exception("Expecting end() of " . get_class($widget) . ", found " . get_called_class());
 			}
 		} else {
-			throw new InvalidCallException("Unexpected " . get_called_class() . '::end() call. A matching begin() is not found.');
+			throw new \Exception("Unexpected " . get_called_class() . '::end() call. A matching begin() is not found.');
 		}
 	}
-	
+
 	/**
 	 * Creates a widget instance and runs it.
 	 * The widget rendering result is returned by this method.
@@ -65,8 +75,48 @@ class Widget
 	    ob_implicit_flush(false);
 	    /** @var Widget $widget */
 	    $config['class'] = get_called_class();
-	    $widget = Yii::createObject($config);
+	    $widget = self::createObject($config['class']);
 	    $out = $widget->run();
 	    return ob_get_clean() . $out;
+	}
+	
+	/**
+	 * Creates a new object using the given configuration.
+	 *
+	 * The configuration can be either a string or an array.
+	 * If a string, it is treated as the *object class*; if an array,
+	 * it must contain a `class` element specifying the *object class*, and
+	 * the rest of the name-value pairs in the array will be used to initialize
+	 * the corresponding object properties.
+	 *
+	 * The method will pass the given configuration as the last parameter of the constructor,
+	 * and any additional parameters to this method will be passed as the rest of the constructor parameters.
+	 *
+	 * @param string|array $config the configuration. It can be either a string representing the class name
+	 * or an array representing the object configuration.
+	 * @return mixed the created object
+	 */
+	public static function createObject($config)
+	{
+	    static $reflections = [];
+	
+	    if (is_string($config)) {
+	        $class = $config;
+	        $config = [];
+	    } elseif (isset($config['class'])) {
+	        $class = $config['class'];
+	        unset($config['class']);
+	    } else {
+	        throw new \Exception('Object configuration must be an array containing a "class" element.');
+	    }
+
+	    $class = new $class;
+	    foreach($config as $key => $value)
+	    {
+	        if(isset($class->$key))
+	            $class->$key = $value;
+	    }
+
+	    return $class;
 	}
 }
